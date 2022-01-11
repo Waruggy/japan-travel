ca62be9d2059f7e254e1fc7f47a49b4640f65802
Waruggy <48106241+Waruggy@users.noreply.github.com>
2022-01-11 03:23:55 -0800

    Add files via upload

---
+++ components/CovidCases.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <h1 class="m">Covid Cases of Japanese Prefectures</h1>
+    <ul>
+      <li class="covidcases">
+        <b>{{ name_en }}:</b>
+        {{getCases(name_en.cases , name_en.deaths)}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    };
+  },
+  name: "CovidCases",
+  // Set component props
+  props: {
+    prefecture: {
+      required: true,
+    },
+  },
+  methods: {
+  getCases(cases, deaths) {
+  if (cases + death > 10000){
+    return name_en.cases + name_en.death + " Danger!";
+  } else {
+    return name_en.cases + name_en.death + " Safe!";
+  }
+  },
+  },
+};
+</script>
+
+
+<style lang="scss">
+// Store page content
+
+.covidcases {
+  display: block;
+}
+
+@import "~/node_modules/bootstrap/scss/bootstrap.scss";
+@import "../scss/_base.normalize.scss";
+@import "../scss/_components.content.scss";
+@import "../scss/_settings.responsive.scss";
+@import "../scss/_settings.variables.scss";
+@import "../scss/style.scss";
+</style>
